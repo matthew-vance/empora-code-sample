d6b7b2edf89d02503f44c8ec57426cf15cfdd70b
Matthew Vance <mavance44@gmail.com>
2024-02-16 08:02:11 -0500

    create basic cli interface

--- cli/cli.test.ts
+++ cli/cli.test.ts
@@ -1,9 +1,26 @@
-import { describe, it } from "vitest";
+import { afterEach, describe, expect, it } from "vitest";
 
 import { run } from "./cli";
 
 describe("cli", () => {
-  it("runs", async () => {
-    run();
+  const originalArgv = process.argv;
+
+  afterEach(() => {
+    process.argv = originalArgv;
+  });
+
+  it("should run with argument", async () => {
+    process.argv = process.argv.slice(0, 2).concat(["test"]);
+    await run();
+  });
+
+  it("should not run without argument", async () => {
+    process.argv = process.argv.slice(0, 2);
+    await expect(run()).rejects.toThrowError();
+  });
+
+  it("should run with --help", async () => {
+    process.argv = process.argv.slice(0, 2).concat(["--help"]);
+    await run();
   });
 });
--- cli/cli.ts
+++ cli/cli.ts
@@ -1,3 +1,21 @@
-export function run() {
-  console.log("cli");
+import cac from "cac";
+
+const cli = cac();
+cli.help();
+
+cli.command("<file>", "").action((file) => {
+  console.log("file", file);
+});
+
+export async function run() {
+  try {
+    cli.parse(process.argv, { run: false });
+    await cli.runMatchedCommand();
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+
+    throw error;
+  }
 }
